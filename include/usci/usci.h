44e33d5daa5239a16debbe1b2977046efe085f38
Markus Fischer <markus.fischer@uni-jena.de>
2022-12-14 13:16:53 +0100

    type safety due removal of preprocessor macro

--- include/usci/usci.h
+++ include/usci/usci.h
@@ -13,8 +13,14 @@
 
 namespace msp430hal::usci
 {
+    enum class UsciModule
+    {
+        usci_a, usci_b
+    };
+
     namespace
     {
+
         static constexpr volatile uint8_t* usci_a_reg[][7] = {
                 {&UCA0CTL0, &UCA0CTL1, &UCA0BR0, &UCA0BR1, &UCA0STAT, &UCA0RXBUF, &UCA0TXBUF},
         #ifdef __MSP430_HAS_USCI_AB1__
@@ -29,24 +35,28 @@ namespace msp430hal::usci
         #endif
         };
 
-        #define USCI_REGISTER(module, instance, reg_num) (module == UsciModule::usci_a ? usci_a_reg : usci_b_reg)[instance][reg_num]
+        template<UsciModule module, int instance>
+        constexpr volatile uint8_t* getUsciRegister(int reg_no)
+        {
+            if constexpr(module == UsciModule::usci_a)
+                return usci_a_reg[instance][reg_no];
+            else
+                return usci_b_reg[instance][reg_no];
+        }
     }
 
-    enum class UsciModule
-    {
-        usci_a, usci_b
-    };
+
 
     template<const UsciModule usci_module, const int instance>
     struct Usci_t
     {
-        static constexpr volatile std::uint8_t* ctl_0 = USCI_REGISTER(usci_module, instance, 0);
-        static constexpr volatile std::uint8_t* ctl_1 = USCI_REGISTER(usci_module, instance, 1);
-        static constexpr volatile std::uint8_t* br_0 = USCI_REGISTER(usci_module, instance, 2);
-        static constexpr volatile std::uint8_t* br_1 = USCI_REGISTER(usci_module, instance, 3);
-        static constexpr volatile std::uint8_t* stat = USCI_REGISTER(usci_module, instance, 4);
-        static constexpr volatile std::uint8_t* rx_buf = USCI_REGISTER(usci_module, instance, 5);
-        static constexpr volatile std::uint8_t* tx_buf = USCI_REGISTER(usci_module, instance, 6);
+        static constexpr volatile std::uint8_t* ctl_0 = getUsciRegister<usci_module, instance>(0);
+        static constexpr volatile std::uint8_t* ctl_1 = getUsciRegister<usci_module, instance>(1);
+        static constexpr volatile std::uint8_t* br_0 = getUsciRegister<usci_module, instance>(2);
+        static constexpr volatile std::uint8_t* br_1 = getUsciRegister<usci_module, instance>(3);
+        static constexpr volatile std::uint8_t* stat = getUsciRegister<usci_module, instance>(4);
+        static constexpr volatile std::uint8_t* rx_buf = getUsciRegister<usci_module, instance>(5);
+        static constexpr volatile std::uint8_t* tx_buf = getUsciRegister<usci_module, instance>(6);
 
         static inline void enableModule()
         {
