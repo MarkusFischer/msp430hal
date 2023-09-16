62b52380ad78e2e1e7c7fa97ee87dc6d3bb93d8b
Markus Fischer <markus.fischer@uni-jena.de>
2023-09-16 16:25:35 +0200

    std::lock_guard like wrapper to disable (and reenable) interrupts

---
+++ include/msp430hal/multitasking/interrupt_guard.h
@@ -0,0 +1,27 @@
+#ifndef MSP430HAL_MULTITASKING_INTERRUPT_MUTEX_H
+#define MSP430HAL_MULTITASKING_INTERRUPT_MUTEX_H
+
+#include <msp430.h>
+
+namespace msp430hal
+{
+    namespace multitasking
+    {
+        struct InterruptGuard
+        {
+            InterruptGuard()
+            {
+                __disable_interrupt();
+            }
+
+            ~InterruptGuard()
+            {
+                __enable_interrupt();
+            }
+
+            void operator=(InterruptGuard& other) = delete;
+        }
+    }
+}
+
+#endif //MSP430HAL_MULTITASKING_INTERRUPT_MUTEX_H
