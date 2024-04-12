aded7f3cbf05ecf640edac2aeaee6ac12d1d5626
Markus Fischer <markus.fischer@uni-jena.de>
2024-04-12 19:19:48 +0200

    documented CPU namespace

--- include/msp430hal/cpu/clock_module.h
+++ include/msp430hal/cpu/clock_module.h
@@ -7,7 +7,7 @@ namespace msp430hal
 {
     namespace cpu
     {
-        /// \brief this enumeration encodes the available clocks
+        /// \brief This enumeration encodes the available clocks that can be configured with functions in this namespace.
         enum Clock
         {
             mclk, ///< main clock
@@ -15,14 +15,15 @@ namespace msp430hal
             aclk ///< auxillary clock
         };
 
+        /// \brief Encodes the possible sources for the clock.
         enum ClockSource
         {
-            dcoclk,
-            xt2clk,
-            xt1clk
+            dcoclk, ///< Internal digitally controlled oscillator (DCO).
+            xt2clk, ///< Optional high-frequency oscillator
+            xt1clk ///< external low-frequency/high-frequency oscillator or external clock
         };
 
-        /// \brief specifies the prescaler used by the clock
+        /// \brief Specifies the pre-scaler used by the clock
         enum Divider
         {
             times_1 = 0b00, ///< divide speed of clock source by 1
@@ -31,33 +32,43 @@ namespace msp430hal
             times_8 = 0b11 ///< divide speed of clock source by 8
         };
 
+        /// \brief Encodes the frequency range of a external HF clock source.
         enum HighFrequencyRange
         {
-            range_0MHz4_to_1MHz = 0b00,
-            range_1MHz_to_3MHz = 0b01,
-            range_3MHz_to_16MHZ = 0b10,
-            range_external = 0b11
+            range_0MHz4_to_1MHz = 0b00, ///< 400 kHz - 1 MHz crystal or resonator
+            range_1MHz_to_3MHz = 0b01, ///< 1 MHz - 3 MHz crystal or resonator
+            range_3MHz_to_16MHZ = 0b10, ///< 3 MHz - 16 MHz crystal or resonator
+            range_external = 0b11 ///< Digital external clock source 400 kHz - 16 MHz
         };
 
+        /// \brief Encodes the external low frequency clock source.
         enum LowFrequencySource
         {
-            watch_crystal = 0,
-            vloclk = 0x20,
-            external_clock = 0x30
+            watch_crystal = 0, ///< external 32768 Hz crystal
+            vloclk = 0x20, ///< internal very low-power low-frequency oscillator
+            external_clock = 0x30 ///< digital external clock source
         };
 
+        /// \brief Encodeds which internal oscillator load capacitors can be used together with a external low frequency crystal
         enum OscillatorCapacitor
         {
-            approx_1pF = 0,
-            approx_6pF = 0x04,
-            approx_10pF = 0x08,
-            approx_12pF5 = 0x0c
+            approx_1pF = 0, ///< approximately 1 pF
+            approx_6pF = 0x04, ///< approximately 6 pF
+            approx_10pF = 0x08, ///< approximately 10 pF
+            approx_12pF = 0x0c ///< approximately 12 pF
         };
 
+        /// \brief Basic template for clock source selection
+        ///
+        /// \tparam clock the clock for which the clock source should be selected
+        /// \param source the clock source that should be used by clock
         template<const Clock clock>
         inline void selectClockSource(ClockSource source)
         {}
 
+        /// \brief template specialization for clock source selection of the MCLK
+        ///
+        /// \param source the clock source that should be used by the MCLK
         template<>
         inline void selectClockSource<Clock::mclk>(ClockSource source)
         {
@@ -75,6 +86,9 @@ namespace msp430hal
             }
         }
 
+        /// \brief template specialization for clock source selection of the SMCLK
+        ///
+        /// \param source the clock source that should be used by the SMCLK
         template<>
         inline void selectClockSource<Clock::smclk>(ClockSource source)
         {
@@ -84,20 +98,29 @@ namespace msp430hal
                 BCSCTL2 |= 0x08;
         }
 
+        /// \brief Deactivate the optional XT2 oscillator.
         inline void turnXT2Off()
         {
             BCSCTL1 |= 0x80;
         }
 
+        /// \brief Activate the optional XT2 oscillator.
         inline void turnXT2On()
         {
             BCSCTL1 &= 0x7f;
         }
 
+        /// \brief Template dummy to select the frequency range for a given high frequency clock.
+        ///
+        /// \tparam source the clock source for which the frequency range should be specified
+        /// \param range the frequency range of the clock source
         template<const ClockSource source>
         inline void setFrequencyRange(HighFrequencyRange range)
         {}
 
+        /// \brief Template specialization for the XT2CLK to select the frequency range.
+        ///
+        /// \param range The frequency range of XT2CLK.
         template<>
         inline void setFrequencyRange<ClockSource::xt2clk>(HighFrequencyRange range)
         {
@@ -105,6 +128,9 @@ namespace msp430hal
             BCSCTL3 |= range << 6;
         }
 
+        /// \brief Template specialization for the XT1CLK to select the frequency range.
+        ///
+        /// \param range The frequency range of XT1CLK.
         template<>
         inline void setFrequencyRange<ClockSource::xt1clk>(HighFrequencyRange range)
         {
@@ -113,6 +139,9 @@ namespace msp430hal
             BCSCTL3 |= range << 4;
         }
 
+        /// \brief select the clock source for the low frequency range.
+        ///
+        /// \param source The low frequency clock source.
         inline void setLowFrequencySource(LowFrequencySource source)
         {
             BCSCTL1 &= 0xbf;
@@ -120,16 +149,26 @@ namespace msp430hal
             BCSCTL3 |= source;
         }
 
+        /// \brief Select which internal load capacitors should be used with the low frequency clock.
+        ///
+        /// \param capacitor the capacitor value to be used
         inline void selectOscillatorCapacitor(OscillatorCapacitor capacitor)
         {
             BCSCTL3 &= 0xf3;
             BCSCTL3 |= capacitor;
         }
 
+        /// \brief Template dummy to select the pre-scaler value for the clock.
+        ///
+        /// \tparam clock the clock for which the prescaler should be modified.
+        /// \param divider Specifies the prescaler value.
         template<const Clock clock>
         inline void setInputDivider(Divider divider)
         {}
 
+        /// \brief Template specialization to select prescaler for the MCLK.
+        ///
+        /// \param divider Prescaler value used by MCLK
         template<>
         inline void setInputDivider<Clock::mclk>(Divider divider)
         {
@@ -137,6 +176,9 @@ namespace msp430hal
             BCSCTL2 |= divider << 4;
         }
 
+        /// \brief Template specialization to select prescaler for the SMCLK.
+        ///
+        /// \param divider Prescaler value used by SMCLK
         template<>
         inline void setInputDivider<Clock::smclk>(Divider divider)
         {
@@ -144,6 +186,9 @@ namespace msp430hal
             BCSCTL2 |= divider << 1;
         }
 
+        /// \brief Template specialization to select prescaler for the ACLK.
+        ///
+        /// \param divider Prescaler value used by ACLK
         template<>
         inline void setInputDivider<Clock::aclk>(Divider divider)
         {
@@ -152,18 +197,21 @@ namespace msp430hal
         }
 
 #ifndef __DisableCalData
+        /// \brief possible calibrated frequency values that can be used by the internal DCO.
         enum CalibratedDCOFrequency
         {
-            calibrated_1MHz,
-            calibrated_8MHz,
-            calibrated_12MHz,
-            calibrated_16MHz
+            calibrated_1MHz, ///< 1 MHz
+            calibrated_8MHz, ///< 8 MHz
+            calibrated_12MHz, ///< 12 MHz
+            calibrated_16MHz ///< 16 MHz
         };
 
+        /// \brief Template dummy to set the calibrated frequency used by the internal DCO.
         template<CalibratedDCOFrequency frequency>
         inline void setCalibratedFrequency()
         {}
 
+        /// \brief Set the Frequency used by the internal DCO to 1 MHz.
         template<>
         inline void setCalibratedFrequency<calibrated_1MHz>()
         {
@@ -172,6 +220,7 @@ namespace msp430hal
             DCOCTL = CALDCO_1MHZ;
         }
 
+        /// \brief Set the Frequency used by the internal DCO to 8 MHz.
         template<>
         inline void setCalibratedFrequency<calibrated_8MHz>()
         {
@@ -180,6 +229,7 @@ namespace msp430hal
             DCOCTL = CALDCO_8MHZ;
         }
 
+        /// \brief Set the Frequency used by the internal DCO to 12 MHz.
         template<>
         inline void setCalibratedFrequency<calibrated_12MHz>()
         {
@@ -188,6 +238,7 @@ namespace msp430hal
             DCOCTL = CALDCO_12MHZ;
         }
 
+        /// \brief Set the Frequency used by the internal DCO to 16 MHz.
         template<>
         inline void setCalibratedFrequency<calibrated_16MHz>()
         {
--- include/msp430hal/cpu/flash_controller.h
+++ include/msp430hal/cpu/flash_controller.h
@@ -12,6 +12,10 @@ namespace msp430hal
     {
         namespace
         {
+            /// \brief Return the right FSSEL configuration bits depending on the clock that will be used.
+            ///
+            /// \param clock Clock that will be used.
+            /// \return FSSEL bits for clock.
             inline constexpr std::uint16_t clockFSSELValue(Clock clock)
             {
                 if (clock == Clock::aclk)
@@ -23,25 +27,40 @@ namespace msp430hal
             }
         }
 
-
+        /// \brief The password that has to be set in the upper byte when writing to flash.
         const std::uint16_t flash_password = FWKEY;
 
+        /// \brief Select the clock that will be used by the flash controller.
+        ///
+        /// \param clock The clock that will be used.
         inline void selectFlashClock(Clock clock)
         {
             FCTL2 = flash_password | clockFSSELValue(clock) | (FCTL2 & 0x3f);
         }
 
+        /// \brief Select the clock that will be used by the flash controller. The clock speed will be divided by a pre scaler.
+        ///
+        /// \param clock The clock that will be used.
+        /// \param divider The prescaler value resulting out of the lower 6 bits of divider + 1. The clock speed will be divided by this value.
         inline void selectFlashClock(Clock clock, std::uint8_t divider)
         {
             FCTL2 = flash_password | clockFSSELValue(clock) | (divider & 0x3f);
         }
 
+        /// \brief set the prescaler for the flashcontroller.
+        ///
+        /// \param divider The prescaler value resulting out of the lower 6 bits of divider + 1. The clock speed will be divided by this value.
         inline void setFlashClockDivider(std::uint8_t divider)
         {
             FCTL2 = flash_password | (FCTL2 & 0x00c0) | (divider & 0x3f);
         }
 
 
+        /// \brief Erase a whole segment at the address
+        ///
+        /// \param segment_address The address of the segment that will be erased.
+        /// \param watchdog_active Is the watchdog active? If true, it will be stopped and (re)enabled afterwards.
+        /// \return false if some error occurred, true otherwise
         inline bool eraseSegment(std::uint16_t segment_address, bool watchdog_active = false)
         {
             if (watchdog_active)
@@ -69,6 +88,13 @@ namespace msp430hal
             return true;
         }
 
+        /// \brief Write arbitrary data to the flash at a given address.
+        ///
+        /// \tparam T The type of the data which should be written to flash for size determination.
+        /// \param address The address where the write operation should occure.
+        /// \param data The data which should be written to flash.
+        /// \param watchdog_active Is the watchdog active? If true, it will be stopped and (re)enabled afterwards.
+        /// \return false if some error occurred, true otherwise
         template<typename T>
         bool write(std::uint16_t address, T data, bool watchdog_active = false)
         {
@@ -92,6 +118,12 @@ namespace msp430hal
             return true;
         }
 
+        /// \brief Write a byte to the flash memory.
+        ///
+        /// \param address The address where the write operation should occure.
+        /// \param byte The byte which should be written to flash.
+        /// \param watchdog_active Is the watchdog active? If true, it will be stopped and (re)enabled afterwards.
+        /// \return false if some error occurred, true otherwise
         inline bool writeByte(std::uint16_t address, std::uint8_t byte, bool watchdog_active = false)
         {
             if (watchdog_active)
@@ -114,6 +146,12 @@ namespace msp430hal
             return true;
         }
 
+        /// \brief Write a word to the flash memory.
+        ///
+        /// \param address The address where the write operation should occure.
+        /// \param word The word which should be written to flash.
+        /// \param watchdog_active Is the watchdog active? If true, it will be stopped and (re)enabled afterwards.
+        /// \return false if some error occurred, true otherwise
         inline bool writeWord(std::uint16_t address, std::uint16_t word, bool watchdog_active = false)
         {
             if (watchdog_active)
